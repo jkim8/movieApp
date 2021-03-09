60864564931a0ced4eaad299ff0cce7be17ea5f5
naujkim <jkim@pbsus.com>
2021-03-09 12:57:16 -0500

    add grid card

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,4 +3,6 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
 
+export const API_KEY = '302eb9909c18dbe310619aa2bb5c8b12';
+
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
