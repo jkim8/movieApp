ac633d2cdec2eb01381fcaedcbaeecb8ee8c8e58
naujkim <jkim@pbsus.com>
2021-03-11 17:19:15 -0500

    favoritePage

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -4,6 +4,7 @@ import MainImage from '../LandingPage/Sections/MainImage'
 import MovieInfo from './Sections/MovieInfo'
 import GridCards from '../commons/GridCards'
 import { Row } from 'antd'
+import Favorite from './Sections/Favorite'
  
 function MovieDetail(props) {
 
@@ -56,6 +57,11 @@ function MovieDetail(props) {
             {/* Body */}
 
             <div style={{ width:'85%', margin: '1rem auto' }} >
+                {/* Favorite Button */}
+                
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }} >
+                    <Favorite movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem('userId')} />
+                </div>
 
                 {/* Movie Info */}
                 <MovieInfo 
